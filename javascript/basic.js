9e3beb0d95aff9c2f9bc89a81b306eef67bc8364
harishkumar <harishrajendirancse@gmail.com>
2025-01-10 11:39:37 +0530

    Opacity image

---
+++ javascript/basic.js
@@ -0,0 +1,13 @@
+obj={
+    firstname:"harish",
+    lastname:"kumar"
+}
+console.log(obj);
+console.log(obj.firstname);
+console.log(obj["lastname"]);``
+
+array=[7,'h',"harish",[01,02]]
+obj1={}
+obj2=new Object()
+console.log(obj1);
+
